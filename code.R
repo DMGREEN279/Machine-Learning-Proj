a2b2b1ca3c927c00ec3abaae93d3f571b4ad4c3d
DMGREEN279 <dav.green279@gmail.com>
2021-09-08 19:49:55 -0400

    visualizations continue

--- code.R
+++ code.R
@@ -11,6 +11,7 @@ library('GGally')
 library('e1071')
 library('gridBase')
 library('gridExtra')
+library('rlang')
 train <-read.csv('C:/Users/dmg27/Desktop/MLproject/train.csv', stringsAsFactors = F)
 test <-read.csv('C:/Users/dmg27/Desktop/MLproject/test.csv', stringsAsFactors = F)
 
@@ -58,5 +59,30 @@ cattrain<-train[Train_char]
 
 #visualize
 fourbar(cattrain, fun = plotHist, ii = 1:4, ncol = 2)
+
+fourbar(cattrain, fun = plotHist, ii = 5:8, ncol = 2)
+
+fourbar(cattrain, fun = plotHist, ii = 9:12, ncol = 2)
+
+fourbar(cattrain, fun = plotHist, ii = 13:18, ncol = 2)
+
+fourbar(cattrain, fun = plotHist, ii = 19:22, ncol = 2)
+
+#boxplots for salesprices
+ggplot(train, aes(x = Neighborhood, y = SalePrice)) +
+  geom_boxplot() +
+  geom_hline(aes(yintercept=80), 
+             colour='blue', linetype='solid', lwd=2) +
+  scale_y_continuous(labels=dollar_format()) +
+  theme_few()
+
+#plots for numerical
+fourbar(numtrain, fun = plotDen, ii = 2:6, ncol = 2)
+
+fourbar(numtrain, fun = plotDen, ii = 7:12, ncol = 2)
+
+fourbar(numtrain, fun = plotDen, ii = 13:17, ncol = 2)
+
+fourbar(numtrain, fun = plotDen, ii = 18:23, ncol = 2)
 
 
--- plot shortcuts.R
+++ plot shortcuts.R
@@ -3,6 +3,23 @@ plots
 
 ## Density with bar
 
+plotHist <- function(data_in, i) 
+{
+  data <- data.frame(x=data_in[[i]])
+  p <- ggplot(data=data, aes(x=factor(x))) + stat_count() + xlab(colnames(data_in)[i]) + theme_light() + 
+    theme(axis.text.x = element_text(angle = 90, hjust =1))
+  return (p)
+}
+
+## Density plot function
+
+plotDen <- function(data_in, i){
+  data <- data.frame(x=data_in[[i]], SalePrice = data_in$SalePrice)
+  p <- ggplot(data= data) + geom_line(aes(x = x), stat = 'density', size = 1,alpha = 1.0) +
+    xlab(paste0((colnames(data_in)[i]), '\n', 'Skewness: ',round(skewness(data_in[[i]], na.rm = TRUE), 2))) + theme_light() 
+  return(p)
+}
+
 fourbar <- function(data_in, fun, ii, ncol=3) 
 {
   pp <- list()
