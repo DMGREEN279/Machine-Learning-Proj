b64b204cea4218236d07b74ca3918c81151eb22a
DMGREEN279 <dav.green279@gmail.com>
2021-09-08 15:57:52 -0400

    Xpl data

--- code.R
+++ code.R
@@ -14,4 +14,24 @@ test <-read.csv('C:/Users/dmg27/Desktop/MLproject/test.csv', stringsAsFactors = 
 dim(train)
 
 
-#test
+str(train)
+
+dim(test)
+str(test)
+
+#how many cols are text data
+
+sum(sapply(train[,1:81], typeof) == "character")
+
+# how many cols are numerical data
+
+sum(sapply(train[,1:81], typeof) == "integer")
+
+summary(train[,sapply(train[,1:81], typeof) == "num"])
+
+test$SalePrice<-rep(NA,1459)
+comptest<-bind_rows(train,test)
+
+## lets see what it looks like
+
+str(comptest)
